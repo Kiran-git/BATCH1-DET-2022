4bbac42082de7a2ae063020e85a1a1b0cde1547a
ETG1 <ETG1@6C3XQG2-SHEL>
2022-08-30 18:05:54 -0700

    commit all

---
+++ BATCH1-DET-2022/CSFeatures.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BATCH1_DET_2022
+{
+    internal class CSFeatures
+    {
+        public static void Main()
+        {
+            Action<string> action=(string name) => { Console.WriteLine($"hai {name}"); };
+            Action<string, string> action1=(string s1, string s2) => { Console.WriteLine($"hai {s1}," + $"you said {s2}"); };
+
+            action.Invoke("marry");
+            action1.Invoke("marry", "am in blr");
+        }
+    }
+}
---
+++ MyClassLibrary/ICar.cs
@@ -0,0 +1,24 @@
+﻿namespace MyClassLibrary
+{
+    public class Icar
+    {
+       
+        
+            public int GetPrice(String name)
+            {
+                if (name == "i10")
+                {
+                    return 2000000;
+                }
+                else if (name == "i20")
+                {
+                    return 2500000;
+                }
+                else
+                    return 1500000;
+            }
+
+        
+
+    }
+}
